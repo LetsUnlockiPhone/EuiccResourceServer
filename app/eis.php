2007ba7579fd454a2bc514f76a1f70973e83b51d
jk204 <martin.jolette@yahoo.fr>
2015-11-14 13:22:21 +0100

    Update Model

--- app/eis.php
+++ app/eis.php
@@ -4,21 +4,57 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class eis extends Model
+/**
+ * This Laravel Model classes handles the EIS model
+ * 
+ * @package    EuiccResourceServer
+ * @subpackage Models
+ * @author github.com/jk204
+ *
+ */
+class Eis extends Model
 {
 	protected $table = 'eis';
 	protected $primaryKey = 'eid';
 	
-	
 	function getEisFromEid($eid){
 		
-		$eis = eis::find($eid);
+		$eis = Eis::find($eid);
 		
 		return $eis;
 	}
 	
-	function profileinfo(){
+	/**
+	 * This function establishes relational link with ProfileInfo Model
+	 * 
+	 * @param none
+	 * @return Model ProfileInfo 
+	 */
+	function profileInfo()
+	{
 		
-		$this-hasMany('\App\profileinfo', 'eid', 'eid');
+		return $this-hasMany('\App\ProfileInfo', 'eid', 'eid');
 	}
+	
+	/**
+	 * Establishes relationship with Euicc Capabilities Model
+	 * 
+	 * @return Model EuiccCapabilities
+	 */
+	function euiccCapabilities()
+	{
+
+		return $this-hasMany('\App\EuiccCapabilities', 'eid', 'eid');
+	}
+	
+	/**
+	 * Establishes relationship with Audit Trail Model
+	 * 
+	 * @return Model AuditTrail
+	 */
+	function auditTrail()
+	{
+		return $this-hasMany('\App\AuditTrail', 'eid', 'eid');
+	}
+	
 }
